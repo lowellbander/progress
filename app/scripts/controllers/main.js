7662fbad25321242e084898adacff330e41b92ce
Lowell Bander <lowellbander@gmail.com>
2014-09-03 13:23:43 -0700

    can edit range in app

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -14,11 +14,25 @@ angular.module('progressApp')
       'AngularJS',
       'Karma'
     ];
-    $scope.first = 1092;
-    $scope.last = 1113;
+
+    $scope.dispSettings = false;
+
+    $scope.first = 1157;
+    $scope.last = 1195;
     $scope.current = $scope.first;
     $scope.$watch('current', function(){
         $scope.total = $scope.last - $scope.first;
+        $scope.completed = $scope.current - $scope.first;
+        $scope.remaining = $scope.total - $scope.completed;
+        $scope.percent = Math.round(100 * ($scope.completed / $scope.total));
+        $scope.message = $scope.percent == 100 ? "Congratulations! You're done!" : "Good work! Keep going!"
+        if ($scope.percent < 0)
+            $scope.percent = '...'
+    });
+    $scope.$watch('first', function(){
+        $scope.current = $scope.first;
+    });
+    $scope.$watch('last', function(){
         $scope.completed = $scope.current - $scope.first;
         $scope.remaining = $scope.total - $scope.completed;
         $scope.percent = Math.round(100 * ($scope.completed / $scope.total));
